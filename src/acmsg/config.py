95d3952a771266d59c2631871d91adb2d220672b
Quinn Edenfield <quinn@qeden.dev>
2025-04-17 01:49:48 -0700

    fix(git_utils,config): fix bugs in GitUtils.is_git_repo() and cli arg parsing

--- src/acmsg/config.py
+++ src/acmsg/config.py
@@ -6,8 +6,8 @@ class Config:
     def __init__(self):
         self.config_dir = self.create_dir_if_not_exists()
         self.config_file = self.create_file_if_not_exists()
-        self.model = self.get_param("model")
-        self.api_token = self.get_param('api_token')
+        self.model = self.get_option("model")
+        self.api_token = self.get_option("api_token")
 
     def create_dir_if_not_exists(self):
         home = os.getenv("HOME") or os.path.expanduser("~")
@@ -26,20 +26,20 @@ class Config:
 
         return config_file
 
-    def set_param(self, param, value):
+    def set_option(self, option, value):
         config_file = self.config_file
         with open(config_file, "r") as f:
             data = yaml.safe_load(f)
 
-        data[param] = value
+        data[option] = value
 
         with open(config_file, "w") as f:
             yaml.dump(data, f, default_flow_style=False)
 
-    def get_param(self, param):
+    def get_option(self, option):
         config_file = self.config_file
 
         with open(config_file, "r") as f:
             data = yaml.safe_load(f)
 
-        return data.get(param)
+        return data.get(option)
--- src/acmsg/git_utils.py
+++ src/acmsg/git_utils.py
@@ -1,32 +1,42 @@
 import subprocess
+import colorama
+from colorama import Fore
+
+colorama.init(autoreset=True)
 
 
 class GitUtils:
     def is_git_repo():
-        output = subprocess.run(
-            ["git", "rev-parse", "--is-inside-work-tree"],
-            capture_output=True, text=True, check=True)
-        return output.stdout.strip()
+        try:
+            output = subprocess.run(
+                ["git", "rev-parse", "--is-inside-work-tree"],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+            return output.stdout.strip()
+        except subprocess.CalledProcessError:
+            print(Fore.RED + "Not a git repository")
 
     def git_status():
         git_status = subprocess.run(
-            ["git", "diff", "--cached", "--name-status"],
-            capture_output=True, text=True)
+            ["git", "diff", "--cached", "--name-status"], capture_output=True, text=True
+        )
         if git_status.returncode != 0:
             raise Exception(f"Git status failed:\n{git_status.stderr}")
         else:
-            return git_status.stdout.strip().replace('\t', ' ').split('\n')
+            return git_status.stdout.strip().replace("\t", " ").split("\n")
 
     def git_diff():
         diff = subprocess.run(
-            ["git", "diff", "--cached"],
-            capture_output=True, text=True)
+            ["git", "diff", "--cached"], capture_output=True, text=True
+        )
         return diff.stdout.strip()
 
     def git_commit(message):
         commit = subprocess.run(
-            ["git", "commit", "-m", message],
-            capture_output=True, text=True)
+            ["git", "commit", "-m", message], capture_output=True, text=True
+        )
         if commit.returncode != 0:
             raise Exception(commit.stderr)
         else:
