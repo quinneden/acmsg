7746bc2040d3e9e813e2205fff5874c5ac0aa8e3
quinneden <quinn@qeden.dev>
2025-05-08 23:11:24 -0700

    fix(script/create-release.nix): use branch variable and fix flag handling
    Updated branch checks to use local variable instead of repeated git calls
    Fixed conditional comparisons to use the new branch variable
    Modified flag addition to properly quote increment argument

--- scripts/create-release.nix
+++ scripts/create-release.nix
@@ -9,20 +9,20 @@ writeShellApplication {
   runtimeInputs = [ commitizen ];
   text = ''
     baseDir=$(git rev-parse --show-toplevel)
-
-    uncommitedChanges=$(git diff --compact-summary)
+    branch=$(git symbolic-ref --short HEAD)
+    uncommittedChanges=$(git diff --compact-summary)
     unpushedCommits=$(git log --format=oneline origin/main..main)
 
-    if [[ $(git rev-parse --show-toplevel) != "$PWD" ]]; then
+    if [[ $baseDir != "$PWD" ]]; then
       echo "This script must be run from the root of the repository" >&2
       exit 1
-    elif [[ "$(git symbolic-ref --short HEAD)" != "main" ]]; then
+    elif [[ $branch != "main" ]]; then
       echo "must be on main branch" >&2
       exit 1
-    elif [[ -n "$uncommitedChanges" ]]; then
-      echo -e "There are uncommited changes, exiting:\n$uncommittedChanges" >&2
+    elif [[ -n "$uncommittedChanges" ]]; then
+      echo -e "There are uncommitted changes, exiting:\n$uncommittedChanges" >&2
       exit 1
-    elif [[ "$unpushed_commits" != "" ]]; then
+    elif [[ -n "$unpushedCommits" ]]; then
       echo -e "\nThere are unpushed changes, exiting:\n$unpushedCommits" >&2
       exit 1
     fi
@@ -30,7 +30,7 @@ writeShellApplication {
     currentVersion=${version}
     latestReleaseTag=$(git tag --sort=-creatordate | grep -E "^v[0-9]+.[0-9]+.[0-9]+$" | head -n 1)
 
-    if [[ "v$currentVersion" != $latestReleaseTag ]]; then
+    if [[ "v$currentVersion" != "$latestReleaseTag" ]]; then
       echo "error: the version in pyproject.toml doesn't match the latest release tag: $currentVersion != $latestReleaseTag" >&2
       exit 1
     fi
@@ -43,7 +43,7 @@ writeShellApplication {
     while [[ $# -gt 0 ]]; do
       case "$1" in
         major|minor|patch)
-          flags+=(--increment "$1")
+          flags+=("--increment" "$1")
           shift
           ;;
         *)
